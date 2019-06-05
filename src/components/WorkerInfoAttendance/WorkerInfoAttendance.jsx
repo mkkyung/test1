6b01c64037d4621fb216cf3c1796b86d6f6d4729
mkkyung <cng9323@gmail.com>
2019-06-05 13:28:39 +0900

    component

---
+++ src/components/WorkerInfoAttendance/WorkerInfoAttendance.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainWorkerInfoAttendance from 'components/UI/Main/MainWorkerInfoAttendance';
+import MainContentsSubHeader from 'components/UI/Main/MainContentsSubHeader';
+
+const WorkerInfoAttendance = () => {
+  return (
+    <>
+      <MainContentsSubHeader/>
+      <MainWorkerInfoAttendance/>
+    </>
+  )
+}
+export default WorkerInfoAttendance;
