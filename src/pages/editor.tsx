2b7a1982c5022ed47d9c4362947cfdd3ea46f466
mkkyung <cng9323@gmail.com>
2019-06-05 14:01:24 +0900

    env

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from 'components/UI/Layout';
 import Editor from 'components/Editor';
 
-const editor = () => {
+const editor: React.FC = () => {
   return (
     <Layout pageType = "Editor">
       <Editor/>
