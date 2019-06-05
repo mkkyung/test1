ab411440617971f796c994fbdae6441b8e8b43ef
Changhun Lee <wakie9292@gmail.com>
2019-06-05 13:49:32 +0900

    sdf

---
+++ src/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { hot } from 'react-hot-loader'
+const Root: React.FC = () => {
+  return (
+    <div/>
+  );
+}
+
+export default (Root);
